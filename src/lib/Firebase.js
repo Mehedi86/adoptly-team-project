769083666bfa0ad1017d85f92bbfcf1472a7dbde
kalidashodekare14 <kalidashodekare0@gmail.com>
2025-10-03 21:49:31 +0600

    update .env config file name

--- src/lib/Firebase.js
+++ src/lib/Firebase.js
@@ -6,12 +6,12 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: process.env.API_KEY,
-    authDomain: process.env.AUTH_DOMAIN,
-    projectId: process.env.PROJECT_ID,
-    storageBucket: process.env.STORE_BUCKET,
-    messagingSenderId: process.env.MESSAGING_SENGDER_ID,
-    appId: process.env.APP_ID
+    apiKey: process.env.NEXT_PUBLIC_API_KEY,
+    authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
+    projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
+    storageBucket: process.env.NEXT_PUBLIC_STORE_BUCKET,
+    messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_SENGDER_ID,
+    appId: process.env.NEXT_PUBLIC_APP_ID
 };
 
 // Initialize Firebase
