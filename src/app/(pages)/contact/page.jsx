499bf2471f6455acaa5957c85388c9abd2b6780e
afrin823 <afrin12490@gmail.com>
2025-10-03 21:48:02 +0600

    contact us page upadeted

--- src/app/(pages)/contact/page.jsx
+++ src/app/(pages)/contact/page.jsx
@@ -2,10 +2,12 @@ import React from 'react';
 import ReachUs from './(components)/reachUs';
 import Map from './(components)/map';
 import ContactUs from './(components)/contact';
+import AboutHero from '../about/(component)/aboutHero';
 
 const page = () => {
     return (
         <div>
+            <AboutHero title={"Contact Us"} />
             <ReachUs />
             <Map />
             <ContactUs />
