731c12db9155efe56ac619ef546dc488db535207
jubayer718 <jubayerahmed2462@gmail.com>
2025-10-04 07:32:02 +0600

    user login system done

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,7 +2,10 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import AuthProvider from "@/context/AuthContext";
 import { queryClient } from "@/lib/tanstackQuery/queryClient";
+//tanstack query
 import { QueryClientProvider } from "@tanstack/react-query";
+//react hot toast
+import { Toaster } from "react-hot-toast";
 
 
 
@@ -28,7 +31,8 @@ export default function RootLayout({ children }) {
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
        <QueryClientProvider client={queryClient}>
-        <AuthProvider>
+          <AuthProvider>
+            <Toaster />
           {children}
         </AuthProvider>
         </QueryClientProvider>
