a8c17ff7db2fc1a683292e02750af03ee153b4e0
kalidashodekare14 <kalidashodekare0@gmail.com>
2026-02-10 01:08:24 +0600

    Home page Blog section design and button linked

--- src/app/(pages)/blog/page.jsx
+++ src/app/(pages)/blog/page.jsx
@@ -1,18 +1,15 @@
-import React from 'react';
-import BlogHero from './component/blogHero'
-import BlogCard from './component/blogCards'
-import UserProtectedRoute from '@/components/protectedRoute/UserProtectedRoute';
-
+import React from "react";
+import BlogHero from "./component/blogHero";
+import BlogCard from "./component/blogCards";
+import UserProtectedRoute from "@/components/protectedRoute/UserProtectedRoute";
 
 const blogPage = () => {
-    return (
-       
-              <div>
-            <BlogHero title={"Blog & Article"} />
-            <BlogCard />
-        </div>
-      
-    );
+  return (
+    <div>
+      <BlogHero title={"Blog & Article"} />
+      <BlogCard />
+    </div>
+  );
 };
 
 export default blogPage;
